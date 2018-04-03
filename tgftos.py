c6ecda251825e497319d1fe53be3642806ad80ae
Manish Yadav <yadavmanish1607@gmail.com>
2018-04-03 19:11:17 +0530

    Update tgftos.py

--- tgftos.py
+++ tgftos.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 
 for chapter_number in range(1, 22):
-    chapter_number = 20.6
+    #chapter_number = 20.6
     page = requests.get('http://mangakakalot.com/chapter/totsukuni_no_shoujo/chapter_' + str(chapter_number))
     soup = BeautifulSoup(page.content, 'html.parser')
     images = soup.select("#vungdoc img")
